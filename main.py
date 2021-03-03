4b5984255c3dba5322ddcfb50b6161aed20c4281
Nicolas MF <nikolamf@hotmail.com>
2021-03-03 11:00:53 +0100

    Added gitignore + Fixed Warnings

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 ## =============================================== M A I N =============================================================
 # Import
 import tkinter as tk
-# execute the other files
+# Execute the other files
 exec(open("./src/functions.py").read())
 exec(open("./src/class.py").read())
 
-# Cretion of the window and the canvas
-window =tk.Tk()
+# Creation of the window and the canvas
+window = tk.Tk()
+
+window.protocol("WM_DELETE_WINDOW", window.destroy())
 
 # Dimension
 width = 1000
-height= 700
+height = 700
 dimension = str(width) +"x"+ str(height)
 window.geometry(dimension)
 
@@ -18,19 +20,19 @@ window.geometry(dimension)
 window.title("The_Kitchen_Project")
 
 # Canvas
-can=tk.Canvas(window,bg="white",width=width,height=height)
-can.place(x=0,y=0)
+can = tk.Canvas(window,bg="white",width=width,height=height)
+can.place(x = 0,y = 0)
 
 # To activate binding
 #can.bind("<Button-1>",clic)
 
 # Global variables to be define
-    #img
+    # img
 searchIcon = tk.PhotoImage(file = r"img/magnifier_icon.png").subsample(20, 20)
 searchBar = tk.Entry(window, width = 70, justify = "center")
 buttonSearch = tk.Button(window, image = searchIcon, background = "white", borderwidth = 0, highlightthickness = 0, command = SBDelete)
 
-# LAunch first function (mainmenu)
+# Launch first function (mainmenu)
 mainmenu(width, height)
 
 # Mainloop
--- src/functions.py
+++ src/functions.py
@@ -4,7 +4,7 @@ File that contains all the functions.
 
 Typo of a function::
 def function():
-    " " "Function that creates a function that make somthing cool
+    " " "Function that creates a function that make something cool
     " " "
     #Global Variables
 
@@ -15,33 +15,39 @@ def function():
     #Core
     (...)
 """
-##import
+# Imports
 import main as m
 import tkinter as tk
 
+# Solve warning "global declared variable is not declared in the global scope"
+searchBar = None
+buttonSearch = None
+searchIcon = None 
+can = None
+
 ## ======================================================  M a i n m e n u  ======================================================
 def mainmenu(width, height):
-    """Function that creates and places the main menu, with its button and of the text that comes with it.
+    """Function that creates and places the main menu, with its button and the text that comes with it.
     """
     # Variables
         # Global
     global searchBar, buttonSearch, searchIcon
     # Core
-        #up border
-    m.can.create_rectangle(0,0,width-2,height*0.15, fill='orange',outline='white')
-        #title
-    m.can.create_text(width*0.5, height*0.075, text="The Kitchen Project", fill="black", font=("Calibri light",30))
-        #menu
+        # Up border
+    #m.can.create_rectangle(0,0,width-2,height*0.15, fill = 'orange', outline = 'white')
+        # Title
+    #m.can.create_text(width*0.5, height*0.075, text = "The Kitchen Project", fill = "black", font = ("Calibri light",30))
+        # Menu
     #m.can.create_text(width*0.25, height*0.7, text="Search a Recipe", fill="black", font=("Calibri",20), anchor="n",justify="center")
     #m.can.create_text(width/2, height*0.4, text="Create a schedule\n(complete meal)", fill="black", font=("Calibri",20), anchor="n",justify="center")
     #m.can.create_text(width*0.75, height*0.7, text="New Ideas\nExplore", fill="black", font=("Calibri",20), anchor="n",justify="center")
-        # search bar
+        # Search bar
     searchBar.pack()
     searchBar.place(x = width*0.25, y = height*0.15 , height = 40, width= height*0.7)
     searchBar.focus()
-        # image
+        # Image
     buttonSearch.pack()
-    buttonSearch.place(x = width*0.75, y = height*0.15+5)
+    buttonSearch.place(x = width*0.71, y = height*0.15+5)
     m.window.bind('<Return>', SBDelete) # Binding return key to SBDelete function
 
 def deleteCanvas():
@@ -50,7 +56,7 @@ def deleteCanvas():
     global can
     can.delete("all")
 
-#Search bar main menu
+# Search bar main menu
 def SBDelete(*args): # *args to fix window.bind('<Return>', SBDelete) (Will raise an error otherwise) 
         searchBar.delete(0, "end")
         return None
@@ -72,11 +78,12 @@ def SBDelete(*args): # *args to fix window.bind('<Return>', SBDelete) (Will rais
 def clic(event):
     """ Function that dispaches what to launch in function of what is activated and how
     """
-    #Global Variables
+    # Global Variables
 
-    #Variables
+    # Variables
     X=event.x
     Y=event.y
-    #New variables
 
-    #Core
+    # New variables
+
+    # Core
