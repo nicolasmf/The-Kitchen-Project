27fd36cf9b64b113c5f64686fe01aae2f3aebe57
Nicolas MF <nikolamf@hotmail.com>
2021-03-03 11:32:16 +0100

    Added gitignore + Fixed warning

--- src/functions.py
+++ src/functions.py
@@ -15,8 +15,15 @@ def function():
     #Core
     (...)
 """
-##import
+# Import
 import tkinter as tk
+
+# Fix warning "global variable is not defined in the global scope"
+can = None
+searchBar = None
+buttonSearch = None
+searchIcon = None
+window = None
 
 ## ======================================================  M a i n m e n u  ======================================================
 def mainmenu(width, height):
@@ -24,24 +31,24 @@ def mainmenu(width, height):
     """
     # Variables
         # Global
-    global can, searchBar, buttonSearch, searchIcon
+    global can, searchBar, buttonSearch, searchIcon, window
     # Core
-        #up border
-    can.create_rectangle(0,0,width-2,height*0.20, fill='orange',outline='white')
-        #title
-    can.create_text(width*0.5, height*0.05, text="The Kitchen Project", fill="black", font=("Calibri light",30))
-        #menu
+        # Up border
+    can.create_rectangle(0, 0, width-2, height*0.20, fill = 'orange', outline = 'white')
+        # Title
+    can.create_text(width*0.5, height*0.05, text = "The Kitchen Project", fill = "black", font = ("Calibri light",30))
+        # Menu
     #m.can.create_text(width*0.25, height*0.7, text="Search a Recipe", fill="black", font=("Calibri",20), anchor="n",justify="center")
     #m.can.create_text(width/2, height*0.4, text="Create a schedule\n(complete meal)", fill="black", font=("Calibri",20), anchor="n",justify="center")
     #m.can.create_text(width*0.75, height*0.7, text="New Ideas\nExplore", fill="black", font=("Calibri",20), anchor="n",justify="center")
-        # search bar
+        # Search bar
     searchBar.pack()
-    searchBar.place(x = width*0.25, y = height*0.13 , height = 40, width= height*0.7)
-    #\|/ Focus pas obligatoire à l'arrivé du mainmenu
+    searchBar.place(x = width*0.25, y = height*0.13 , height = 40, width = height*0.7)
+    # \|/ Focus pas obligatoire à l'arrivé du mainmenu
     #searchBar.focus() 
-        # image
+        # Image
     buttonSearch.pack()
-    buttonSearch.place(x = width*0.75, y = height*0.13)
+    buttonSearch.place(x = width*0.71, y = height*0.14)
     window.bind('<Return>', SBDelete) # Binding return key to SBDelete function
 
 def deleteCanvas():
@@ -50,7 +57,7 @@ def deleteCanvas():
     global can
     can.delete("all")
 
-#Search bar main menu
+# Search bar main menu
 def SBDelete(*args): # *args to fix window.bind('<Return>', SBDelete) (Will raise an error otherwise) 
         searchBar.delete(0, "end")
         return None
@@ -72,11 +79,11 @@ def SBDelete(*args): # *args to fix window.bind('<Return>', SBDelete) (Will rais
 def clic(event):
     """ Function that dispaches what to launch in function of what is activated and how
     """
-    #Global Variables
+    # Global Variables
 
-    #Variables
-    X=event.x
-    Y=event.y
-    #New variables
+    # Variables
+    X = event.x
+    Y = event.y
+    # New variables
 
-    #Core
+    # Core
